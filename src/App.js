87c747f133b589b788539c8a5954e95f3193080b
Ian Pous <pousig@g.crookcountyschools.org>
2022-03-08 17:43:43 -0800

    Lab 08

--- src/App.js
+++ src/App.js
@@ -1,63 +1,69 @@
+
 import React from 'react';
 import './App.css';
 import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Alert from 'react-bootstrap/Alert';
-import Weather from './Weather';
-
+import Weather from './weather';
 class App extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       city: '',
-    locationObj: {},
-    showError: false,
-    errorMessage: "",
-    weatherArr: []
+      locationObj: {},
+      showError: false,
+      errorMessage: "",
+      weatherArr: []
     };
   }
 
-   handleChange = (event) => {
+
+
+  handleChange = (event) => {
     let typedCity = event.target.value;
     this.setState({ city: typedCity });
     console.log(typedCity);
   };
-  
   getLocation = async (event) => {
     event.preventDefault();
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`;
-    console.log('URL:', url);
+    console.log('URL:', url)
     try {
       let response = await axios.get(url);
-      console.log('Response: ', response.data[0]);
+      console.log('Response:', response.data[0]);
       this.setState({
-      locationObj: response.data[0]
-    });
-    this.getWeather();
-  } catch(error) {
-    this.setState({
-      showError: true,
-      errorMessage: error.response.status + ': ' + error.response.data.error
-    })
-  }
- }
+        locationObj: response.data[0]
+      });
+      this.getWeather();
+    } catch (error) {
+      this.setState({
+        showError: true,
+        errorMessage: error.response.status + ': ' + error.response.data.error
+      })
+    }
 
-getWeather = async () => {
-  const url = `http://localhost:3001/weather?lat=${this.state.locationObj.lat}&lon=${this.state.locationObj.lon}&searchQuery=${this.state.city}`
-  try {
-    let response = await axios.get(url);
-    console.log('weather response: ', response.data);
-    this.setState({
-      weatherArr: response.data
-    });
-  } catch(error) {
-  this.setState({
-  showError: true,
-  errorMessage: 'City Unavailable'
-    })   
   }
-}
+  getWeather = async () => {
+    const url = `${process.env.REACT_APP_SERVER}/weather?lat=${this.state.locationObj.lat}&lon=${this.state.locationObj.lon}&searchQuery=${this.state.city}`
+    try {
+      let response = await axios.get(url);
+      console.log('weather response: ', response.data);
+      this.setState({
+        weatherArr: response.data
+      });
+    } catch (error) {
+      // let errorResponse - await axios.get(url);
+      this.setState({
+        showError: true,
+        errorMessage: 'something went wrong'
+      })
+
+    }
+  }
+
+
 
   render() {
     return (
@@ -65,7 +71,7 @@ getWeather = async () => {
         <header className="App-header">
           <h1>City Explorer</h1>
         </header>
-        <form className= 'form' onSubmit={this.getLocation}>
+        <form className='form' onSubmit={this.getLocation}>
 
           Your City: {" "}
           <input type="text" name="yourcity" onChange={this.handleChange} />
@@ -75,19 +81,19 @@ getWeather = async () => {
           <Container className='container'>
             <h2>here is the map for {this.state.locationObj.display_name} </h2>
             <p>Lat/Lon: {this.state.locationObj.lat}, {this.state.locationObj.lon}</p>
-            <Image className='image' src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationObj.lat},${this.state.locationObj.lon}&zoom=12 `} alt={this.state.locationObj.display_name}/>
-            <Weather weatherArr={this.state.weatherArr}/>
-            </Container>
-  }
-  {this.state.showError && 
-  
-<Alert variant='danger'>{this.state.errorMessage}</Alert>
-  }
+            <Image className='image' src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationObj.lat},${this.state.locationObj.lon}&zoom=12 `} alt={this.state.locationObj.display_name} />
+            <Weather weatherArr={this.state.weatherArr} />
+          </Container>
+        }
+        {this.state.showError &&
 
-    </div>
-  );
+          <Alert variant='danger'>{this.state.errorMessage}</Alert>
+        }
+
+      </div>
+    );
+  }
 }
-      }
-  
-    
+
+
 export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Card from "react-bootstrap/Card";
+import { Row } from 'react-bootstrap';
+import WeatherDay from './weatherday.js';
 
 class Weather extends React.Component {
     render() {
         return (
             <Row md={3}>
-               {this.props.weatherArr.map((day, idx) => (
-                <Col key={idx}>
-                    <Card className="forecast">
-                        <Card.Text>Date: {day.day}</Card.Text>
-                        <Card.Text>Forecast: {day.description}</Card.Text>
-                    </Card>
-                </Col>
+                {this.props.weatherArr.map((dayObj, idx) => (
+                    <WeatherDay dayObj = {dayObj} idx={idx}/>
+
                 ))}
             </Row>
         )
